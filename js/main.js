d8b7f3c1f37672e6f6b9c2739a3ed004bf2418b3
JohannMG <hello@johannmg.com>
2015-08-25 21:51:28 -0400

    first

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+var CLOCK; //for interval refresh; 
+var REFRESH_RATE = 60; //fps
+var REFRESH_INTERVAL = Math.floor(1000 / 60);  //frequency of updates per seconds to hit the DESIRED refresh LIMIT
+
+
+//INITIAL SET
+document.addEventListener("DOMContentLoaded", function(event) {
+
+});
